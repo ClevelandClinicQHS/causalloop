881b028a6af0548bf3ac77a8c0f2116691071f11
jarrod-dalton <daltonj@Jarrods-MacBook-Pro.local>
2018-08-01 13:50:07 -0400

    particle-based loop search
    Johnson’s algorithm tended to be computationally infeasible for even
    moderately-sized CLDs. So I went with a particle-based simulation
    approach.

--- R/getLoops.R
+++ R/getLoops.R
@@ -3,6 +3,12 @@
 #' @title Extract, Plot and Manipulate Labels for Feedback Loops in a Causal Loop Diagram
 #'
 #' @param x a CLD object
+#' @param S number of loop traversals (random walks on directed graph) to be simulated,
+#'   originating from each variable in the CLD.
+#' @param nsteps number of steps for each loop traversal. Setting this to larger values
+#'   (e.g., 10 or 15) tends to yield a larger number of identified loops, many of which
+#'   involve large numbers of variables. The default value of 6 tends to yield simpler
+#'   loop structures (which contain up to a maximum of 6 variables).
 #' @param FBL A \code{feedbackLoops} object (i.e., object outputted from \code{getLoops()})
 #' @param loopIx Loop index. When \code{getLoops()} is run, each loop is assigned an index.
 #'   The function \code{setLoopLabel} will take a \code{loopIx} as input. The loop indices
@@ -16,17 +22,11 @@
 #'   number of links with negative polarity.
 #' @export
 #'
-#' @details This function applies Johnson's algorithm for finding cycles
-#'   in a directed graph. In particular, we adapt for the \code{causalloop}
-#'   package Jacobien Carstens' implementation of Johnson's algorithm,
-#'   which can be found on GitHub at the link below (see References)
+#' @details This function applies a particle-based approach to identifying loops
+#'   (cycles) within a causal loop diagram.  Random walks are generated, initiated at
+#'   each variable, and any cycles that are identified are retained and numbered.
 #'
 #' @references
-#'   Johnson, D. B. (1975). Finding all the elementary circuits of a
-#'   directed graph. SIAM Journal on Computing, 4(1), 77-84.
-#'
-#'   Carstens, J. R implementation of Johnson's algorithm.
-#'   \url{https://github.com/queenBNE/DirectedGraphsCycles}
 #
 #'
 #' @examples
@@ -53,14 +53,8 @@
 #' loops <- getLoops(z)
 #' loops <- setLoopLabel(loops, loopIx = 5, label = "BCFdE!")
 #'
-getLoops <- function(x){
-
-  # make:
-  #  1) a lookup table from node labels in x$edges to integer nodeIDs; and
-  #  2) an igraph object with the graph structure (index by the integer nodeIDs)
-  # if(!causalloop:::allEdfNodesListedInNdf(x)) {
-  #   stop("The 'edges' table in the CLD object contains nodes not in the 'nodes' table.")
-  # }
+getLoops <- function(x, S = 100, nsteps=6){
+  stopifnot("CLD" %in% class(x))
   nodeIxTable <- dplyr::select(x$nodes, node) %>%
     dplyr::group_by(node) %>%
     dplyr::filter(dplyr::row_number() == 1) %>%
@@ -76,131 +70,77 @@ getLoops <- function(x){
     dplyr::rename(toID = nodeID)
 
   nodeID_graph <- igraph::graph_from_data_frame(edf[,c("fromID","toID")])
-
-
-  # This function, adapted from Carstens' R implementation of Johnson's algorithm,
-  # computes the "elementary" cycles of a directed graph g.
-  get_elementary_cycles <- function(g){
-    #Subroutine to obtain the strong component of a graph g with the lowest-numbered
-    #vertex in the subgraph of g induced by vertices {s, s+1, ... n}. The algorithm
-    #is given in Johnson (1975) (see references).
-    get.induced.strong <- function(g, s){
-      # Create the induced subgraph on {s, s+1, ..., n} and compute the strong components
-      sg <- igraph::induced.subgraph(g, vids=s:igraph::vcount(g))
-      sc <- igraph::clusters(sg, mode="strong")
-
-      # Obtain the names for the remaining nodes - this has to be done to make sure that we use
-      # the right order of nodes, we want to find the strong component with the least vertex.
-      # Igraph always uses ids 1:n' for a graph, so we need to use the names.
-      ids <- as.numeric(igraph::get.vertex.attribute(sg, "name", 1:igraph::vcount(sg)))
-      order <- sort(ids, index.return=TRUE)$ix
-
-      # Obtain the vertices of the strong component with the least vertex
-      others <- c()
-      for(v in order)
-        if(length(others) <= 1)
-          others <- which(sc$membership == sc$membership[v])
-
-      # If there is a strong component with more than 1 vertex, return this component
-      if(length(others) > 1)
-        return(igraph::induced.subgraph(sg, others))
-      # Else return NULL
-      else
-        return(NULL)
-    }
-
-    # Subroutine returning a list where u is unblocked and all vertices in B(u)
-    # are unblocked (recursively)
-    unblock <- function(u, b, B){
-      b[u] <- FALSE
-      for(w in B[[u]]){
-        B[[u]] <- B[[u]][-which(B[[u]]==w)]
-        if(b[w]){
-          bB <- unblock(w,b,B)
-          b <- bB$b
-          B <- bB$B
-        }
-      }
-      return(list(b=b, B=B))
-    }
-
-    # Subroutine to produce the circuits (cycles) starting at vertex s
-    circuit <- function(s, v, Ak, B, b, f, stack, ids){
-      stack <- c(stack, v)
-      b[v] <- TRUE
-      for(w in igraph::neighbors(Ak, v, mode="out")){
-        if(w==s){
-          cat(sapply(c(stack,s), FUN=function(i){return(ids[i])}), sep=" ")
-          cat("~")
-          f = TRUE
-        }else if (!b[w]){
-          updated <- circuit(s,w,Ak,B,b,f,stack,ids)
-          B <- updated$B
-          b <- updated$b
-          stack <- updated$stack
-          if(updated$f)
-            f = TRUE
-        }
-      }
-      if(f){
-        updated <- unblock(v, b, B)
-        b <- updated$b
-        B <- updated$B
-      }else{for(w in igraph::neighbors(Ak, v, mode="out"))
-        if (! v %in% B[[w]])
-          B[[w]] <- c(B[[w]], v)
-      }
-      stack <- stack[-length(stack)]
-      return(list(B=B, b=b, f=f, stack=stack))
-    }
-
-    b <- rep(FALSE, igraph::vcount(g))
-    B <- vector("list", igraph::vcount(g))
-    s = 1
-
-    while(s < igraph::vcount(g)){
-      Ak <- get.induced.strong(g,s)
-      if(!is.null(Ak)){
-        ids <- as.numeric(igraph::get.vertex.attribute(Ak, "name", 1:igraph::vcount(Ak)))
-        s <- min(ids)
-        for(i in ids){
-          b[i] <- FALSE
-          B[[i]] <- numeric(0)
-        }
-        s_indx <- which(ids == s)
-        circuit(s_indx, s_indx, Ak, B, b, FALSE, numeric(0), ids)
-        s <- s + 1
-      }else
-        s <- igraph::vcount(g)
-    }
+  L <- purrr::map(nodeIxTable$nodeID, function(x) list(node=x))
+  doWalks <- function(l){
+    l$walks <- plyr::rlply(S,
+                           as.numeric(igraph::random_walk(nodeID_graph,
+                                                          start=l$node,
+                                                          steps=nsteps)))
   }
-  nodeID_graph_cycles <- capture.output(get_elementary_cycles(nodeID_graph))
-  nodeID_graph_cycles <- as.list(strsplit(nodeID_graph_cycles, "~")[[1]])
-  txt2vec <- function(l) strsplit(l, split = " ") %>% unlist() %>% as.numeric()
-  nodeID_graph_cycles <- purrr::map(nodeID_graph_cycles, txt2vec)
-
-  nodeIxTable <- dplyr::select(nodeIxTable, nodeID, from) %>%
-    dplyr::rename(node = from)
-
-  makeCycleListOutput <- function(l){
-    cycle <- tibble::tibble(fromID = l[1:(length(l)-1)],
-                            toID   = l[2:length(l)]) %>%
-      dplyr::left_join(edf, by=c("fromID","toID")) %>%
-      dplyr::select(from, to, polarity)
-    type = dplyr::case_when(prod(cycle$polarity) == -1 ~ "Balancing",
-                            prod(cycle$polarity) == +1 ~ "Reinforcing")
-    return(list(cycle = cycle,
-                type = type))
+  rw <- purrr::map(L, doWalks)
+  rw <- unlist(rw, recursive = FALSE)
+  #trim random walks to contain only cycles
+  trimWalks <- function(v) {
+    endIx <- which(v[-1] == v[1])
+    if(length(endIx)>0) {
+      trimmedWalk <- v[1:(min(endIx)+1)]
+      return(trimmedWalk[!duplicated(trimmedWalk)])
+    } else return(endIx)
   }
-  Result <- purrr::map(nodeID_graph_cycles, makeCycleListOutput)
+  rw <- purrr::map(rw, trimWalks)
+  rw <- rw[purrr::map(rw,length)>0]
+  #standardize all loops so that they begin at the lowest numbered node
+  rotateLoop <- function(v){
+    minIx <- which.min(v)
+    if(minIx == 1) return(v) else return(v[c(minIx:length(v), 1:(minIx-1))])
+  }
+  rw <- purrr::map(rw, rotateLoop)
+  rw <- rw[!duplicated(rw)]
+  #format the output
+  rw <- purrr::map(rw, function(v) c(v, v[1]))
+  rw <- purrr::map2(1:length(rw), rw, ~list(loopIx=..1, loop=..2))
+  rw <- purrr::map(rw, function(l) tibble::tibble(loopIx = l$loopIx, nodeID = l$loop))
+  nodeIDMap <- tibble::tibble(nodeID=1:nrow(nodeIxTable),
+                              actualNodeID = as.numeric(attr(igraph::V(nodeID_graph),
+                                                             "names")))
+  rw <- rw %>% dplyr::bind_rows() %>%
+    dplyr::left_join(nodeIDMap, by = "nodeID") %>%
+    dplyr::select(-nodeID) %>%
+    dplyr::rename(nodeID = "actualNodeID") %>%
+    dplyr::left_join(dplyr::select(nodeIxTable, nodeID, from) %>%
+                       dplyr::rename(var="from"),
+                     by="nodeID")
+  rw <- rw %>%
+    dplyr::group_by(loopIx) %>%
+    dplyr::mutate(from=var, to=lead(var)) %>%
+    dplyr::filter(!is.na(to)) %>%
+    dplyr::select(loopIx, from, to) %>%
+    dplyr::left_join(select(edf, from, to, polarity), by=c("from", "to"))
+  rw <- dplyr::left_join(rw, (rw %>% dplyr::summarize(anyNA = max(is.na(polarity)))),
+                         by="loopIx") %>%
+    dplyr::filter(anyNA == 0) %>%
+    dplyr::select(-anyNA) %>%
+    dplyr::ungroup() #%>%
+  #mutate(lagLoopIx = lag(loopIx), leadLoopIx = lead(loopIx))
+  #correct loop indices so that they are consecutive
+  loopIxMap <- tibble::tibble(loopIx = sort(unique(rw$loopIx))) %>%
+    dplyr::mutate(newLoopIx = 1:n())
+  nloops <- nrow(loopIxMap)
+  rw <- dplyr::left_join(loopIxMap, rw, by = "loopIx") %>%
+    dplyr::select(-loopIx) %>%
+    dplyr::rename(loopIx = newLoopIx)
+  rw <- split(rw, rw$loopIx)
 
-  tmp <- purrr::map2(as.list(1:length(Result)), as.list(rep("",length(Result))),
+  tmp <- purrr::map2(as.list(1:nloops), as.list(rep("",nloops)),
                      function(.x, .y) list(loopIx = .x, label = .y))
-  Result <- purrr::map2(tmp, Result,
-                        function(.x, .y) list(loopIx = .x$loopIx,
-                                              label  = .x$label,
-                                              type   = .y$type,
-                                              cycle  = .y$cycle))
+  makeResult <- function(.x, .y){
+    loopIx <- .x$loopIx
+    label <- .x$label
+    type <- ifelse(prod(.y$polarity) == -1, "Balancing", "Reinforcing")
+    loop <- dplyr::select(.y, from, to, polarity)
+    list(loopIx = loopIx, label = label, type = type, loop = loop)
+  }
+  Result <- purrr::map2(tmp, rw, makeResult)
   class(Result) <- "feedbackLoops"
   Result
 }
